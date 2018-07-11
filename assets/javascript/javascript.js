21a44b96aefc5e8867812cb26b06951f81a9f857
SpencerC <sweetwater55@hotmail.com>
2018-07-11 18:35:02 -0500

    GipghyApp

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -37,7 +37,7 @@ $( document ).ready(function() {
 
     function displayGifs(){
         var action = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=a4cBau4QhzNrdfN7llgoD8I9VO1uCQbt&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=a4cBau4QhzNrdfN7llgoD8I9VO1uCQbt&limit=10";
         $.ajax({
             url: queryURL,
             method: 'GET'
